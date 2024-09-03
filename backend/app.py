6ec55a45e7f64febce84be8a1bc9b2e5829f7d57
奕翔 錢 <st10083010@gmail.com>
2024-09-04 02:09:39 +0800

    add react files

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # 設定CORS, 處理跨域問題
 
-@app.route('/<path>')
+@app.route('/')
 def home():
-    return 'home page'
+    result = {
+        "status": 0,
+        "msg": "Great success, here is home page."
+    }
+
+    return jsonify(result)
+
+@app.route('/api/hello')
+def hello():
+    result = {
+        "status": 0,
+        "msg": "here is hello api"
+    }
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
